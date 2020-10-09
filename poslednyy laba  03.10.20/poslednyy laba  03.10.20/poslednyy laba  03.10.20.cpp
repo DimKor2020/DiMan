8bab81d61fac8dbc8cd21211343b5eee0adf1256
Dmitry <ucb123@mail.ru>
2020-10-09 23:23:41 +0600

    poslednya zanyatie 3.10.2020

---
+++ poslednyy laba  03.10.20/poslednyy laba  03.10.20/poslednyy laba  03.10.20.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int* begin = array;
+    int* end = array + sizeof(array) / sizeof(*array) - 1;
+
+    while (begin < end) {
+        *begin ^= *end ^= *begin ^= *end;
+        begin++; end--;
+    }
+    for (const int* p = array; p < array + sizeof(array) / sizeof(*array); p++) {
+        cout << *p << " ";
+    }
+}
